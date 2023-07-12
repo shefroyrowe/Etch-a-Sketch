2372847448bbd2702b15069a233d01207252c517
shefroyrowe <shefroy.rowe@gmail.com>
2023-07-11 20:45:22 -0500

    function to generate grid squares

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//grid generator
+function makeGrid(sqrs){
+    for(i = 0; i < sqrs; i++){
+        const wrapper = document.querySelector('.grid-wrap');
+        const cell = document.createElement('div');
+        cell.classList.add('item');
+        wrapper.appendChild(cell);
+    }
+}
+
+//grid generator call
+makeGrid(256); 
+
+
+
