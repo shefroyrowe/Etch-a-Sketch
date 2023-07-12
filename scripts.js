5d473bd32266378dcda77f448e1edfd734b469b9
shefroyrowe <shefroy.rowe@gmail.com>
2023-07-12 15:25:44 -0500

    fix grid making function > added grid layout buttons added layout choosing logic

--- scripts.js
+++ scripts.js
@@ -1,22 +1,46 @@
 //grid generator
-function makeGrid(sqrs){
-    for(i = 0; i < sqrs; i++){
+function makeGrid(sqrs) {
+    let i = 0;
+    while (i < sqrs) {
         const wrapper = document.querySelector('.grid-wrap');
         const cell = document.createElement('div');
         cell.classList.add('item');
         wrapper.appendChild(cell);
+        i++;
     }
 }
 
-//grid generator call
-makeGrid(256); 
+//makeGrid(16*16); testing makeGrid
 
-//drawing event
-let items = document.querySelectorAll('.item');
-items.forEach(item =>{
-    item.addEventListener('mouseover', ()=>{
-        item.style.cssText = 'background-color: yellow';
-    });
-});
+//makeGrid and drawing event
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let btnId = e.target.id;
+
+        if (btnId === 'grid16') {
+            makeGrid(16 * 16)
+        }if(btnId === 'grid32'){
+            makeGrid(32 * 32);
+        }if(btnId === 'grid64'){
+            makeGrid(64 * 64);
+        }if(btnId === 'grid72'){
+            makeGrid(72 * 72);
+        }if(btnId === 'grid80'){
+            makeGrid(80 * 80);
+        }
+
+        //drawing event
+        const items = document.querySelectorAll('.item');
+        items.forEach(item => {
+            item.addEventListener('mouseover', () => {
+     //item.style.cssText = 'background-color: orangered'; -> turn on!!
+            });
+        });
+    });//end event listener
+});//end for each
+
+
+
 
 
