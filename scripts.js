54a0874ebfa2ccf769db07f2625c6e9d14e5c2fb
shefroyrowe <shefroy.rowe@gmail.com>
2023-07-14 00:57:43 -0500

    fixed grid breaking on options buttons click > feature buttons added > random rainbow coloring complete

--- scripts.js
+++ scripts.js
@@ -11,23 +11,23 @@ function makeGrid(sqrs) {
 }//...
 
 //get layout selecting buttons
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.controls');
 
 //begin canvas control logic 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         let btnId = e.target.id;
 
-        //get grid cells
+        //get grid cell container
         const wrapper = document.querySelector('.grid-wrap');
-        
-        //remove grid cells function
+
+        //remove grid cells > 1/2 parts
         function removeGrid() {
             while (wrapper.firstChild) {
                 wrapper.firstChild.remove()
             }
         }
-        //call to remove grid function
+        //call to remove grid cells 2/2 parts
         if (btnId !== '') {
             removeGrid();
         }
@@ -49,10 +49,11 @@ buttons.forEach(button => {
             makeGrid(100 * 100);
         }//...
 
-        //get grid cells
+
+        //get grid cells for css appending
         const boxes = document.querySelectorAll('.item');
 
-        //css logic to normalize grid cell size
+        //append css to normalize grid cell sizes
         boxes.forEach(box => {
             if (btnId === 'grid16') {
                 box.style.cssText = 'height: 28px; width: 28px;';
@@ -71,28 +72,54 @@ buttons.forEach(button => {
             }
         });//...
 
-        //get grid cells
+        //generate random background color
+        function randomColor() {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.round(Math.random() * 15)];
+            }
+            return color;
+        }//...
+
+        //get rainbow toggle button
+        const rainbow = document.querySelector('#rainbow');
+
+        //get grid cells to apply canvas coloring
         const items = document.querySelectorAll('.item');
 
         //keydown and drag > logic to draw on canvas
-        let mouseDown = false;
+        //begin for...each
+        items.forEach(item => {
+            let rainbow_On = false;
+            rainbow.addEventListener('click', () => {
+                rainbow_On = true;
+            });
 
-            //begin for...each
-            items.forEach(item => {
-                addEventListener('mousedown', ()=>{
-                    mouseDown = true;
-                });
-                addEventListener('mouseup', ()=>{
-                    mouseDown = false;
-                });
-                item.addEventListener('mouseenter', () => {
-                    if(mouseDown){
-                      item.style.background = 'blue';
-                    }
-                });
-            });//end for...each
+            let mouseDown = false;
+            addEventListener('mousedown', () => {
+                mouseDown = true;
+            });
+            addEventListener('mouseup', () => {
+                mouseDown = false;
+            });
+
+            //background color event listener
+            item.addEventListener('mouseenter', () => {
+
+                //default color
+                if(mouseDown){
+                item.style.background = 'salmon';
+                }
+
+                //while rainbow_On is true and mousedown is also true
+                //canvas color mode = random/rainbow color effect
+                if (rainbow_On === true && mouseDown === true) {
+                    item.style.background = randomColor();
+                }
+            });
+        });//end for...each
         //...
-
     });//end canvas control for...each
 });//end canvas control logic 
 
@@ -101,15 +128,14 @@ buttons.forEach(button => {
 //=======================//
 
 //add eraser function
-//add randomized color option
+
+//[add randomized color option (complete)]...
+
 //add color picker
-//add method to clear entire grid without removing cells
 //method to remove grid lines
-//method to gradually increase color opacity
+//method to gradually increase color opacity from light to fully dark
 
 //[method to click and drag to draw (complete)]...
-
-
 
 
 
